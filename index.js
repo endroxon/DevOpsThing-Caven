d42034a1572da5cee5ddb2abf2f21129deb2447c
28-Hazwan <bviper00@gmail.com>
2024-11-07 14:04:21 +0800

    movies main function

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
 const PORT = process.env.PORT || 5050
-var startPage = "index.html";
+var startPage = "movie.html";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const { addResource,viewResources, editResource, deleteResource } = require('./Utils/Resourceutil')
+app.post('/add-resource', addResource);
+app.get('/view-resources', viewResources);
+app.put('/edit-resource/:id', editResource);
+app.delete('/delete-resource/:id', deleteResource);
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
 })
@@ -15,4 +21,4 @@ server = app.listen(PORT, function () {
         address.address}:${address.port}`;
     console.log(`Demo project at: ${baseUrl}`);
 });
-module.exports = { app, server }
+module.exports = { app, server } 
