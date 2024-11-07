39b319b71475875c5f71007bcbb5aa65eff47c54
mamathanif <infernofrostgt@gmail.com>
2024-11-08 01:26:08 +0800

    Implemented 1. Backend, 2. Frontend, 3. Error-Catching for Review Page

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
+
 const PORT = process.env.PORT || 5050
-var startPage = "index.html";
+var startPage = "review.html";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const { addReview, viewReview, editReview, deleteReview  } = require('./utils/ReviewUtil')
+app.post('/add-review', addReview);
+app.get('/view-reviews', viewReview);
+app.put('/edit-review/:id', editReview);
+app.delete('/delete-review/:id', deleteReview);
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
 })
@@ -15,4 +24,5 @@ server = app.listen(PORT, function () {
         address.address}:${address.port}`;
     console.log(`Demo project at: ${baseUrl}`);
 });
+
 module.exports = { app, server }
