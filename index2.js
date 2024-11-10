7018fb3785903038b96eb3da6e4d90d794d1b129
endroxon <167389322+endroxon@users.noreply.github.com>
2024-11-10 14:57:16 +0800

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+
+const PORT = process.env.PORT || 5059
+var startPage = "account.html";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("./public"));
+
+const { createAccount, viewAccounts, updateAccount, deleteAccount  } = require('./utils/AccountUtil.js')
+app.post('/create-account', createAccount);
+
+app.get('/view-accounts', viewAccounts);
+app.put('/update-account/:id', updateAccount);
+app.delete('/delete-account/:id', deleteAccount);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/" + startPage);
+})
+server = app.listen(PORT, function () {
+    const address = server.address();
+    const baseUrl = `http://${address.address == "::" ? 'localhost' :
+        address.address}:${address.port}`;
+    console.log(`Demo project at: ${baseUrl}`);
+});
+
+module.exports = { app, server }
