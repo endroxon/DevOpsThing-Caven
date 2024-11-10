74392ff3388cf9700bdb4bd9942f0da6313adb38
mamathanif <infernofrostgt@gmail.com>
2024-11-10 15:26:15 +0800

    Finalised index.html, complete with succesful merging from each branch

--- index1.js
+++ index1.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
 const PORT = process.env.PORT || 5050
-var startPage = "movie.html";
+var startPage = "index.html";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
@@ -12,6 +12,30 @@ app.post('/add-movie', addMovie);
 app.get('/view-movie', viewMovie);
 app.put('/edit-movie/:id', editMovie);
 app.delete('/delete-movie/:id', deleteMovie);
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("./public"));
+
+const { createAccount, viewAccounts, updateAccount, deleteAccount  } = require('./utils/AccountUtil.js')
+app.post('/create-account', createAccount);
+
+app.get('/view-accounts', viewAccounts);
+app.put('/update-account/:id', updateAccount);
+app.delete('/delete-account/:id', deleteAccount);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("./public"));
+
+const { addReview, viewReview, editReview, deleteReview  } = require('./utils/ReviewUtil')
+app.post('/add-review', addReview);
+app.get('/view-reviews', viewReview);
+app.put('/edit-review/:id', editReview);
+app.delete('/delete-review/:id', deleteReview);
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
 })
