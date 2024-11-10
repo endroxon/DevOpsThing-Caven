06a6e21b53df0631c60b7c1faadd1c1b93e6d507
28_Hazwan <114890871+28-Hazwan@users.noreply.github.com>
2024-11-10 14:32:43 +0800

    Revert "movies merge"

--- index1.js
+++ index1.js
@@ -1,20 +1,17 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
-
 const PORT = process.env.PORT || 5050
-var startPage = "review.html";
-
+var startPage = "movie.html";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
 
-const { addReview, viewReview, editReview, deleteReview  } = require('./utils/ReviewUtil')
-app.post('/add-review', addReview);
-app.get('/view-reviews', viewReview);
-app.put('/edit-review/:id', editReview);
-app.delete('/delete-review/:id', deleteReview);
-
+const { addMovie,viewMovie, editMovie, deleteMovie } = require('./utils/MovieUtils')
+app.post('/add-movie', addMovie);
+app.get('/view-movie', viewMovie);
+app.put('/edit-movie/:id', editMovie);
+app.delete('/delete-movie/:id', deleteMovie);
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
 })
@@ -24,5 +21,4 @@ server = app.listen(PORT, function () {
         address.address}:${address.port}`;
     console.log(`Demo project at: ${baseUrl}`);
 });
-
-module.exports = { app, server }
+module.exports = { app, server } 
