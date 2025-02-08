4d47c84bb4b7f4a0f42963dfcf9c7a549deac374
endroxon <mycaven6804@hotmail.com>
2025-02-08 22:53:14 +0800

    Completed the implementation of real-time monitoring and logging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
+const logger = require('./logger');
 
 const PORT = process.env.PORT || 5054
 var startPage = "account.html";
@@ -8,6 +9,9 @@ var startPage = "account.html";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const statusMonitor = require('express-status-monitor');
+app.use(statusMonitor());
 
 const { createAccount, viewAccounts } = require('./utils/AccountUtil.js')
 app.post('/create-account', createAccount);
@@ -24,6 +28,8 @@ server = app.listen(PORT, function () {
     const baseUrl = `http://${address.address == "::" ? 'localhost' :
         address.address}:${address.port}`;
     console.log(`Demo project at: ${baseUrl}`);
+    logger.info(`Demo project at: ${baseUrl}!`);
+    logger.error(`Example or error log`)
 });
 
 module.exports = { app, server }
